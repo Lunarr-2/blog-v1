340f5df9c4d4cf54abf39a989c0d50808608036f
TAIWO TOLULOPE SHERIFF <oluwalunar.misc@gmail.com>
2026-05-29 20:46:52 +0200

    updated schema

--- schema.py
+++ schema.py
@@ -5,6 +5,10 @@ from typing import Optional
 class PostCreate(BaseModel):
     title : str = Field(...,example="This is Title 1")
     content: str = Field(..., example="This is a content Example")
+    url : str 
+    file_type: str
+    file_name: str
+    
 
 class PostResponse(BaseModel):
     title : str
