b4fa0b1f8bbb8d6c2c8dea5bf892876f89621027
TAIWO TOLULOPE SHERIFF <oluwalunar.misc@gmail.com>
2026-05-30 07:32:28 +0200

    fixed base importing for table name

--- models.py
+++ models.py
@@ -2,14 +2,12 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column,Mapped
 from sqlalchemy import Text, String, Column, UUID, DateTime, ForeignKey
 from datetime import datetime, UTC, timedelta
 import uuid
-
-
-class Base(DeclarativeBase):
-    pass
+from database import Base
 
 
 class Post(Base):
     __tablename__ = "posts"
+
     id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True, nullable=False, default=uuid.uuid4)
     title: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(String, nullable=False)
