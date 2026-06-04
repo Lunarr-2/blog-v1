2488fb07ba1752981d8262a4da575a1c6bef0070
TAIWO TOLULOPE SHERIFF <oluwalunar.misc@gmail.com>
2026-06-04 09:04:28 +0200

    modified schemas

--- schema.py
+++ schema.py
@@ -1,7 +1,25 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 from typing import Optional
 import uuid
 from datetime import datetime
+
+
+class UserBase(BaseModel):
+    username: str = Field(..., example="tolu1")
+    email: EmailStr = Field(..., example="tolu@example.com")
+
+class UserCreate(UserBase):
+    password : str = Field(..., min_length=8, example="password123")
+
+class UserPublic(BaseModel):
+    id: uuid.UUID
+    username: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class UserPrivate(UserPublic):
+    id: uuid.UUID
+    username: str
 
 class PostCreate(BaseModel):
     title : str = Field(...,example="This is Title 1")
@@ -9,6 +27,7 @@ class PostCreate(BaseModel):
     url : str 
     file_type: str
     file_name: str
+    created_at: datetime
     
 
 class PostResponse(BaseModel):
@@ -18,8 +37,10 @@ class PostResponse(BaseModel):
     url: str
     file_type: str
     file_name: str
-    created_at: datetime
+    
 
     model_config = ConfigDict(from_attributes=True)
 
-
+class Token(BaseModel):
+    access_token : str
+    token_type: str = "bearer"
